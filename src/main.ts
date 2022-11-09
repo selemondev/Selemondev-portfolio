7902a89f5e987d157a8e317bfabd5718017a6560
selemondev <selemondev19@gmail.com>
2022-11-09 13:23:50 +0300

    :sparkles: add analytics

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue'
 import VueSmoothScroll from "vue3-smooth-scroll";
 import router from './router';
 import 'aos/dist/aos.css';
+import { inject } from '@vercel/analytics';
+inject();
 const app = createApp(App)
 app.use(VueSmoothScroll);
 app.use(router);
